e788f18e65b4c281978974f8938d39e7d66045c7
Shahriyor <shahriyoradhamov@icloud.com>
2024-09-27 21:53:05 +0500

    feat: implement main game functionalities

--- script.js
+++ script.js
@@ -1,33 +1,80 @@
 'use strict'
-// const winIndex = [
-//     [1,2,3],
-//     [4,5,6],
-//     [7,8,9],
-//     [1,4,7],
-//     [2,5,8],
-//     [3,6,9],
-//     [1,5,9],
-//     [3,5,7]
-// ];
+const winIndex = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7]
+];
 const area = document.querySelector('.game-area');
-const turnBoxes = document.querySelectorAll('.turn-box')
+const turnBoxes = document.querySelectorAll('.turn-box');
+const result = document.querySelector('.result');
+const playAgain = document.querySelector('.play-again');
+
+let xInd = [];
+let oInd = [];
 let boxes = [];
 let turn = 'x';
+let xWin = false;
+let oWin = false;
+let endGame = false;
 
 for(let i = 0; i < 9; i++) {
-    area.innerHTML += "<div class='box' pos=" + i + "></div>";
+    area.innerHTML += "<div class='box' pos=" + (i + 1) + "></div>";
 }
 
 boxes = document.querySelectorAll('.box');
 
+function toVisible() {
+  result.style.display = 'block';
+  playAgain.style.cssText = 'display: block;';
+}
+function gameEnd() {
+  area.style.pointerEvents = 'none';
+}
 boxes.forEach(item => {
   item.addEventListener('click', () => {
     if(item.innerHTML === '') {
       item.innerHTML = turn
+      turn === 'x' ? xInd.push(Number(item.getAttribute('pos'))) : oInd.push(Number(item.getAttribute('pos')));
+      xWin = checkWin(xInd);
+      oWin = checkWin(oInd);
+      if(xWin || oWin || xInd.length === 5) {
+        gameEnd()
+      }
+      if(xWin) {
+        result.innerHTML = 'X win';
+        toVisible();
+      } else if(oWin) {
+        result.innerHTML = 'O win';
+        toVisible();
+      } else if(!oWin && !xWin && xInd.length === 5 ) {
+        result.innerHTML = 'Draw';
+        toVisible();
+      }
       changeTurn();
     }
   })
 })
+
+function restart() {
+  boxes.forEach(item => {
+    item.innerHTML = ''
+  })
+  xInd = [];
+  oInd = [];
+  turn = 'x';
+  xWin = false;
+  oWin = false;
+  turnBoxes[0].classList.add('active-box');
+  turnBoxes[1].classList.remove('active-box');
+  playAgain.style.display = 'none';
+  result.style.display = 'none';
+   area.style.pointerEvents = '';
+}
 
 function changeTurn() {
   if(turn === 'x') {
@@ -40,4 +87,21 @@ function changeTurn() {
   turn = 'x' === turn ? 'o' : 'x';
 }
 
+function checkWin(data) {
+    for(let i in winIndex) {
+        let win = true;
+        for(let j in winIndex[i]) {
+            let id = winIndex[i][j];
+            let ind = data.indexOf(id);
 
+            if(ind == -1) {
+                win = false
+            }
+        }
+
+        if(win) return true;
+    }
+    return false;
+}
+
+playAgain.addEventListener('click', restart)
